4788b84ae8585facf6d5c6670320e88a8ad2aa32
Mirko <mirkmark2@gmail.com>
2018-03-16 13:26:45 +0100

    Everything fixed

--- target/classes/db/migration/V2__init.sql
+++ target/classes/db/migration/V2__init.sql
@@ -1,11 +1,9 @@
 
-DROP TABLE IF EXISTS `app_user`;
-
-CREATE TABLE IF NOT EXISTS `app_user`(
+CREATE TABLE IF NOT EXISTS `user`(
   `id` INT AUTO_INCREMENT,
   `name`VARCHAR(255),
   `email` VARCHAR(2000),
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO app_user(name, email) VALUES ('Mirko', 'mirko@gmail.com');
+INSERT INTO `user` (name, email) VALUES ('Mirko', 'mirko@gmail.com');
