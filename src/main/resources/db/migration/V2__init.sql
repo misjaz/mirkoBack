91d242dd324d51636947cff0294cf4bf5d465c1a
misjaz <mirkmark2@gmail.com>
2018-04-23 00:32:58 +0200

    Dodao kompanije u bazu

--- src/main/resources/db/migration/V2__init.sql
+++ src/main/resources/db/migration/V2__init.sql
@@ -1,9 +1,24 @@
 
-CREATE TABLE IF NOT EXISTS `user`(
-  `id` INT AUTO_INCREMENT,
-  `name`VARCHAR(255),
-  `email` VARCHAR(2000),
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS `company` (
+  `company_id` INT AUTO_INCREMENT,
+  `name` VARCHAR(50),
+  `state` VARCHAR(50),
+  `city` VARCHAR(50),
+  PRIMARY KEY(`company_id`)
 );
 
+CREATE TABLE IF NOT EXISTS `user`(
+  `user_id` INT AUTO_INCREMENT,
+  `name`VARCHAR(255),
+  `email` VARCHAR(2000),
+  `company_id` INT,
+  `state` VARCHAR(50),
+  `city` VARCHAR(50),
+  PRIMARY KEY (`user_id`),
+  FOREIGN KEY (`company_id`) REFERENCES company(`company_id`)
+);
+
+
 INSERT INTO `user` (name, email) VALUES ('Mirko', 'mirko@gmail.com');
+INSERT INTO `company` (name, state, city) VALUES ('Wiener', 'Montenegro', 'Podgorica');
+
--- target/classes/db/migration/V2__init.sql
+++ target/classes/db/migration/V2__init.sql
@@ -1,9 +1,21 @@
 
 CREATE TABLE IF NOT EXISTS `user`(
-  `id` INT AUTO_INCREMENT,
+  `user_id` INT AUTO_INCREMENT,
   `name`VARCHAR(255),
   `email` VARCHAR(2000),
+  `company_id` INT,
+  `state` VARCHAR(50),
+  `city` VARCHAR(50),
   PRIMARY KEY (`id`)
 );
 
+CREATE TABLE IF NOT EXISTS `company` (
+  `company_id` INT AUTO_INCREMENT,
+  `name` VARCHAR(50),
+  `state` VARCHAR(50),
+  `city` VARCHAR(50),
+  PRIMARY KEY(`id`)
+);
+
 INSERT INTO `user` (name, email) VALUES ('Mirko', 'mirko@gmail.com');
+
